d12709b08911ace10457c0176b458e0ddb3e0853
Vraj <pvraj7515@gmail.com>
2025-04-04 19:32:56 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_cloud():
+    return 'Hello from Patel'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
