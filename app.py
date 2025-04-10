8c8af2764af55c365854dd932e9085ed4fa5620b
vraj-patel-exe <72253371+vraj-patel-exe@users.noreply.github.com>
2025-04-09 20:02:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_cloud():
-    return 'Hello from Patel ECS Container'
+    return 'Hello from Patel ECS Container.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
